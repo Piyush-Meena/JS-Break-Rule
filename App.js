803569b34f7e12a9f5fd7c75bb3eb833d4daf5c5
Piyush <94220242+Piyush-Meena@users.noreply.github.com>
2021-11-28 06:35:23 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+let iterations = 0; 
+top: for (let i = 0; i < 5; i++) { 
+        for (let j = 0; j < 5; j++) { 
+            iterations++; 
+            if (i === 2 && j === 2) { 
+                break top; 
+            } 
+        } 
+    } 
+console.log(iterations); // 13
